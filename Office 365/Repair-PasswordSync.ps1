c82d171b90ed3d5ec4421f14af5117b320f7d6f9
Jurgen Dahl <30379381+ErroneousGiant@users.noreply.github.com>
2019-01-09 09:25:28 +0000

    Update Repair-PasswordSync.ps1

--- Office 365/Repair-PasswordSync.ps1
+++ Office 365/Repair-PasswordSync.ps1
@@ -11,12 +11,12 @@ function Repair-PasswordSync {
     $adConnectors = $connectors | Where-Object {$_.ConnectorTypeName -eq "AD"}
     
     if ($null -ne $aadConnectors -and $null -ne $adConnectors) {
-      $c = Get-ADSyncConnector -Name ($adConnectors).Name
-      $p = New-Object Microsoft.IdentityManagement.PowerShell.ObjectModel.ConfigurationParameter "Microsoft.Synchronize.ForceFullPasswordSync", String, ConnectorGlobal, $null, $null, $null
-      $p.Value = 1
-      $c.GlobalParameters.Remove($p.Name)
-      $c.GlobalParameters.Add($p)
-      $c = Add-ADSyncConnector -Connector $c
+      $local = Get-ADSyncConnector -Name ($adConnectors).Name
+      $new = New-Object Microsoft.IdentityManagement.PowerShell.ObjectModel.ConfigurationParameter "Microsoft.Synchronize.ForceFullPasswordSync", String, ConnectorGlobal, $null, $null, $null
+      $new.Value = 1
+      $local.GlobalParameters.Remove($new.Name)
+      $local.GlobalParameters.Add($new)
+      $local = Add-ADSyncConnector -Connector $local
       Set-ADSyncAADPasswordSyncConfiguration -SourceConnector ($adConnectors).Name -TargetConnector ($aadConnectors).Name -Enable $false
       Set-ADSyncAADPasswordSyncConfiguration -SourceConnector ($adConnectors).Name -TargetConnector ($aadConnectors).Name -Enable $true
     }

94200af405874c08b3b0e6f772215060a1c240e1
icebergsynth <46593702+icebergsynth@users.noreply.github.com>
2019-01-11 09:37:06 +0000

    Added Script via Upload
    Added Script via Upload to Enable MultiFactor Authentication on Office 365

---
+++ Office 365/O365MFAEnableAllUsers.ps1
@@ -0,0 +1,17 @@
+﻿$username = Read-Host -Prompt "Username"
+$password = Read-Host -Prompt "Password" -AsSecureString
+$credential = New-Object System.Management.Automation.PSCredential -ArgumentList $username,$password
+
+Connect-MsolService -Credential $credential
+$users = Get-MsolUser -All # | where {$_.isLicensed -eq $true}
+
+$auth = New-Object -TypeName Microsoft.Online.Administration.StrongAuthenticationRequirement
+$auth.RelyingParty = "*"
+$auth.State = "Enabled"
+$auth.RememberDevicesNotIssuedBefore = (Get-Date)
+$auths = @($auth)
+
+foreach ($user in $users)
+{
+    Set-MsolUser -UserPrincipalName $user.UserPrincipalName -StrongAuthenticationRequirements $auths
+}

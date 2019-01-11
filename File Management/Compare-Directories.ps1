c3238162e1677bee7fee9275eb24d75bfc6f5123
Jurgen Dahl <30379381+ErroneousGiant@users.noreply.github.com>
2019-01-11 10:50:49 +0000

    Create Compare-Directories.ps1

---
+++ File Management/Compare-Directories.ps1
@@ -0,0 +1,45 @@
+$sourceArray = @()
+$copyArray = @()
+
+$source = ""   # Example "D:\source"
+$copy = ""   # Example "D:\copy"
+$output = ""   # Example "D:\Comparison.txt"
+
+class file {
+  [string] $fullname
+  [string] $Filename
+  [string] $type
+}
+
+Get-ChildItem -Path $source -Recurse | ForEach {
+    $file = New-Object file
+    $file.fullname = $_.FullName
+    $file.filename = $_.Name
+    if (0 -ne ($_.Extension).length) {
+        $file.type = $_.Extension
+    }
+    else {
+        $file.type = "Directory"
+    }
+
+    $sourceArray += $file
+}
+
+Get-ChildItem -Path $copy -Recurse | ForEach {
+    $file = New-Object file
+    $file.fullname = $_.FullName
+    $file.filename = $_.Name
+    if (0 -ne ($_.Extension).length) {
+        $file.type = $_.Extension
+    }
+    else {
+        $file.type = "Directory"
+    }
+
+    $copyArray += $file
+}
+
+$list = Compare-Object -ReferenceObject $sourceArray -DifferenceObject $copyArray -Property Name -PassThru
+$list | Group-Object -Property type -NoElement
+
+Out-File -FilePath $output -Encoding ascii -InputObject ($list | Sort-Object type | Select-Object fullname, filename, type)
